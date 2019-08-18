4a5e3c9e0b11df1039aa5bf2005ead0c55a3ad30
vishali <uma.ragimanu@gmail.com>
2019-08-18 16:11:32 +1000

    new file added

---
+++ first_script.sh
@@ -0,0 +1,8 @@
+# this script will run at a certain time using crontab job and return to a log file when it is run by user
+#!/bin/bash
+cd /home/umavishalakshi/Project
+echo  `ls`
+echo `date`
+echo `pwd`
+echo "this script will run at a certain time using crontab job and return to a log file when it is run by user"
+
